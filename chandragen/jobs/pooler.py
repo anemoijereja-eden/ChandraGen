d444176ee1b3ecf68f92afe3ea629c9458c68cf3
thanos <thanos@allthingslinux.org>
2025-05-10 12:28:59 -0400

    feat(pooler): add handling for dead workerprocesses - when a worker process crashes, the pooler will remove it from the pool. - the pooler will now enforce the minimum pool size, spawning in the appropriate number of workers if it grows too small.

--- chandragen/jobs/pooler.py
+++ chandragen/jobs/pooler.py
@@ -18,15 +18,15 @@ class WorkerShutdownError(Exception):
         self.reason = reason
         super().__init__(f"Worker {worker_id} failed to shut down cleanly: {reason}")
 
-class WorkerProcess:
+class WorkerProcess(Process):
     def __init__(self, worker_id: UUID, conn: Connection):
+        super().__init__()
         # worker level imports
         from loguru import logger
 
         from chandragen.db.controllers.job_queue import JobQueueController
         from chandragen.jobs.runners import RUNNER_REGISTRY
         
-        logger.debug(f"Starting worker process {worker_id}!")
         self.logger = logger
         self.runners = RUNNER_REGISTRY
         self.job_queue_db = JobQueueController()
@@ -34,10 +34,13 @@ class WorkerProcess:
         self.pipe = conn
         self.running = False
         self.current_job: UUID | None = None
-        self._ipc_thread = threading.Thread(target=self.handle_ipc, daemon=True)
-         
+    
     def setup(self):
-        pass
+        self.running = True
+        self._ipc_thread = threading.Thread(target=self.handle_ipc, daemon=True) 
+        self._ipc_thread.start()
+        logger.debug(f"Starting worker process {self.id}!")
+        self.name = f"chandra_worker_{str(self.id)[:4]}"
     
     def run_job(self, job: tuple[UUID, str]):
         job_id, job_type = job
@@ -82,9 +85,7 @@ class WorkerProcess:
             sleep(0.1) 
     
     def run(self):
-        self.running = True
         self.setup()
-        self._ipc_thread.start()
         while self.running:
             job = self.job_queue_db.claim_next_pending_job(self.id)
             if job:
@@ -94,17 +95,19 @@ class WorkerProcess:
                 self.run_job(job)
                 self.current_job = None
             else:
-                sleep(0.001)
+                # queue miss means we can just go ahead and sleep for a second.
+                logger.debug(f"worker process {self.id} missed queue, resting for a sec")
+                sleep(0.5)
         self.cleanup()
     
     def stop(self):
         self.running = False    
     
 class ProcessPooler:
-    def __init__(self, min_workers: int = 3, max_workers: int = 16, check_interval: float = 0.01):
-        self.min_workers = min_workers
-        self.max_workers = max_workers
-        self.check_interval = check_interval
+    def __init__(self):
+        self.min_workers = system_config.minimum_workers_per_pool
+        self.max_workers = system_config.max_workers_per_pool
+        self.check_interval = system_config.tick_rate
         self.job_queue_db = JobQueueController()
         
         self.workers: dict[UUID, tuple[Process, Connection]] = {}
@@ -116,21 +119,18 @@ class ProcessPooler:
             self.spawn_worker()
         
         while system_config.running:
+            logger.debug("ticking pooler")
+            self.clean_up_dead_workers()
             self.balance_workers()
             sleep(self.check_interval)
         logger.info("chandragen is exiting, shutting down all workers!")
         for worker in list(self.workers.keys()):
            self.stop_worker(worker) 
         
-    def worker_loop(self, worker_id: UUID, conn: Connection):
-        """ Prepares the worker process loop for worker spawning """
-        worker = WorkerProcess(worker_id, conn)
-        worker.run()
-        
     def spawn_worker(self):
         worker_id = uuid4()
         parent_conn, child_conn = Pipe()
-        worker_process = Process(target=self.worker_loop, args=(worker_id, child_conn))
+        worker_process = WorkerProcess(worker_id, child_conn)
         worker_process.start()
         self.workers[worker_id] = (worker_process, parent_conn)
 
@@ -153,12 +153,28 @@ class ProcessPooler:
                 raise WorkerShutdownError(worker_id, reason="Could not kill process! something is very wrong!!") from None
         finally:
                 del self.workers[worker_id]
-
+    
+    def clean_up_dead_workers(self):
+        for worker_id, worker in list(self.workers.items()):
+            worker_process, _worker_connection = worker
+            if worker_process.is_alive():
+                continue
+            logger.warning(f"Found dead worker process {worker_id}, removing from pool.")
+            del self.workers[worker_id]        
+        
     def balance_workers(self):
         """ Checks worker load, adds or removes workers as needed. """
         # Fetch the queue status from the database controller
         _pending_count, in_progress_count, ratio = self.job_queue_db.get_queue_status()
         total_workers = len(self.workers)
+        
+        # aggressively spawn workers to ensure the minimum is met.
+        if total_workers < self.min_workers:
+            logger.warning(f"Worker pool below minimum! ({total_workers} < {self.min_workers})")
+            for _ in range(self.min_workers - total_workers):
+                self.spawn_worker()
+            total_workers = self.min_workers
+        # calculate this after we ensure the pool is filled properly to avoid divide-by-zero
         worker_load_ratio = in_progress_count / total_workers
         
         #if more than 25% of the jobs in the queue are pending, and 80% or more of to workers are going, spawn a worker.
